b75faade9e9ae2b4930b5823842c7c19515bb880
Tom <tom@catcobralizard.com>
2019-05-18 20:04:26 -0700

    pypi distrubution setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="marian",
+    version="0.0.1",
+    author="Tom Spalding",
+    author_email="tom@catcobralizard.com",
+    description="a personal Robinhood API server",
+    url="https://github.com/nebulousdog/marian",
+    license='MIT',
+    classifiers=[
+        "Framework :: Flask",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
