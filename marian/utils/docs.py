19216fc47a93b2f318a8c91356f4bc4505ed3c77
Tom <tom@catcobralizard.com>
2019-05-19 14:25:25 -0700

    add github pages deployment on tagging

--- marian/utils/docs.py
+++ marian/utils/docs.py
@@ -1,2 +1,8 @@
+import docutils.core
+
 def generate_sphinx_docs():
-    pass
+    docutils.core.publish_file(
+        source_path="README.rst",
+        destination_path="./docs/index.html",
+        writer_name="html"
+    )
